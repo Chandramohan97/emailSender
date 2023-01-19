14eb3a459b952394a16cd1f9e02bcf005aacccb0
Chandramohan97 <chandramsriv@gmail.com>
2023-01-19 13:05:13 +0530

    using nodemailer in the entry point of index.js

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 const app = require("express")();
 const path = require("path");
+const nodemailer = require("nodemailer");
+const bodyparse = require("body-parser");
+const { getMaxListeners } = require("process");
 const port = 8080;
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "chandramsriv@gmail.com",
+    pass: "********",
+  },
+});
+
+app.use(bodyparse.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.sendFile(
     "C:/Users/DELL/OneDrive/Desktop/nodePractice/emailSender/index.html"
   );
+});
+
+app.post("/", (req, res) => {
+  const mailDetails = {
+    from: "chandramsriv@gmail.com",
+    to: req.body.mail,
+    subject: "Sending mails using nodeJs",
+    text: "Easy",
+  };
+
+  transporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      res.send("Error occurs");
+    } else {
+      res.send("Email sent successfully");
+    }
+  });
 });
 
 app.listen(port);
